fca6de5999803ecb30f8fcdbf40d4909767a0504
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2023-01-06 07:04:37 +0530

    add: q1 3 and skeleton

---
+++ constants.py
@@ -0,0 +1,6 @@
+import random
+
+SIZE_MATA = (5, 6)
+SIZE_MATB = (6, 9)
+
+SIZE_ARRAY = 51
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+__version__ = "0.0.1"
+
+setup(
+    name="ml-assignment-zero",
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[
+        # "csv==1.0",
+        "numpy"
+    ],
+)
