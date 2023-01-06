488614ef67fa39781913db4cbf9ec802d19cf0c8
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2023-01-06 07:48:51 +0530

    add: 7

--- constants.py
+++ constants.py
@@ -4,3 +4,17 @@ SIZE_MATA = (5, 6)
 SIZE_MATB = (6, 9)
 
 SIZE_ARRAY = 55551
+
+
+ACAD_YEAR = ["2022", "2023", "2024", "2025"]
+MARKS = {
+    "Math": 89,
+    "English": 34,
+    "Sanskrit": 76
+}
+STUDENT = {
+    "roll_number": None,
+    "name": ""
+}
+TOTAL_STUDENTS = 10
+BRANCH = ['Electrical', 'Chemical', 'Materials']
